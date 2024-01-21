91d8310ccb58f5043487d4c2ed2997f00675631b
jaysinghpatel0 <mrjaysingh91@gmail.com>
2024-01-21 17:02:10 +0530

    Adding The Exercise Screen- Intents and Customizing our Toolbar 343

---
+++ app/src/main/java/com/example/a10minutesworkout/ExerciseActivity.kt
@@ -0,0 +1,26 @@
+package com.example.a10minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a10minutesworkout.databinding.ActivityExerciseBinding
+
+class ExerciseActivity : AppCompatActivity() {
+    private var binding: ActivityExerciseBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarExercise)
+
+        if (supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+    }
+}
